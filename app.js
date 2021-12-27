ef338c7e98d6f216105c01d131cac12ad14292f0
vinicius-s-farias <vifarias47@gmail.com>
2021-12-27 16:26:39 -0300

    commit final

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var vm = new Vue({
     data: {
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        monsterLife: 100,
+        logs: []
     },
 
     computed:{
@@ -11,13 +12,46 @@ var vm = new Vue({
             return this.playerLife == 0 || this.monsterLife == 0
         }
     },
-
+    
     methods: {
         startGame(){
             this.running = true,
             this.monsterLife = 100,
-            this.playerLife = 100
+            this.playerLife = 100  ,
+            this.logs = []    
+        },
+        attack(especial){
+            this.hurt('monsterLife', 5, 10, false, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0){
+            this.hurt('playerLife', 7, 12, especial, 'Monstro', 'Jogador', 'monster')
+            }
+        },
+        hurt(atr, min, max, especial, source, target, cls){
+            const plus = especial ? 5 : 0
+            const hurt = this.getRamdom(min + plus, max + plus)
+            this [atr] = Math.max(this[atr] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        getRamdom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        healAndHurt(){
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max ){
+            const heal = this.getRamdom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
+        },
+        registerLog(text, cls){
+            this.logs.unshift({text, cls})
         }
-        
-    }
+    },
+    watch: {
+        hasResult(value){
+            if(value) this.running = false
+        }
+    },
 })
